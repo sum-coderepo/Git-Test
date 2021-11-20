e942add969b00dde5ed6d319271d973306bfc25a
Ashish Chauhan <Ashish.chauhan@students.iiit.ac.in>
2021-11-20 14:50:00 +0530

    Added STATUS and ADD functionality

--- gitAdd.py
+++ gitAdd.py
@@ -1,82 +1,133 @@
-import os        #for is_file(), is_dir(), abspath()
-import pathlib   #for iterdir()
-import hashlib   #for calculating sha256 digest
-import shutil    #for copy()
-import time      #for time()
+import os  # for is_file(), is_dir(), abspath()
+import pathlib  # for iterdir()
+import hashlib  # for calculating sha256 digest
+import shutil  # for copy()
+import time  # for time()
+from colorama import Fore
 
-trackingArea = {}   #for now
-untrackedFiles = {} #for now
-index = {}          #for now
-commitHead = None   #for now
-treeOfCommits = {}  #for now
+trackingArea = {}       # for now
+untrackedFiles = set()  # for now
+trackedFiles = set()    # for now
+modifiedFiles = set()    # for now
+index = {}              # for now
+commitHead = None       # for now
+treeOfCommits = {}      # for now
 
-#at the time of git init, make a set called untrackedFiles
-#add all files in working directory in this set
-#while adding files to trackingArea at the time of git add, strike them out from this set
 
-def shaOf (filename):
+# while adding files to trackingArea at the time of git add, strike them out from this set
+
+
+def shaOf(filename):
     sha256_hash = hashlib.sha256()
-    with open(filename,"rb") as f:
-        for byte_block in iter(lambda: f.read(4096),b""):
+    with open(filename, "rb") as f:
+        for byte_block in iter(lambda: f.read(4096), b""):
             sha256_hash.update(byte_block)
         return sha256_hash.hexdigest()
 
-def addDir (path):
+
+def addDir(path):
     for p in pathlib.Path(path).iterdir():
         if p.is_file() and p not in trackingArea:
-            trackingArea[p] = None
-            print(p,":",trackingArea[p])
+            trackingArea[p] = shaOf(p)
+            trackedFiles.add(p)
+            untrackedFiles.remove(p)
         elif p.is_dir():
-            addDir (p)
+            addDir(p)
 
-def addDirToUntrackedFiles (path):
+
+def addDirToUntrackedFiles(path):
     absolutePath = pathlib.Path(os.path.abspath(path))
     for p in pathlib.Path(absolutePath).iterdir():
         if p.is_file():
             untrackedFiles.add(p)
         elif p.is_dir():
-            addDirToUntrackedFiles (p)
+            addDirToUntrackedFiles(p)
 
-def gitAdd (p):
+
+def gitAdd(p):
+    # p is a list of arguments
     addDirToUntrackedFiles(".")
-    #split p
-    #for element in p
-        absolutePath = pathlib.Path(os.path.abspath(p))
+    for element in p:
+        absolutePath = pathlib.Path(os.path.abspath(element))
         if absolutePath.is_file():
-            trackingArea[absolutePath] = None
+            trackingArea[absolutePath] = shaOf(element)
+            trackedFiles.add(absolutePath)
+            untrackedFiles.remove(absolutePath)
         elif absolutePath.is_dir():
-            addDir (absolutePath)
+            addDir(absolutePath)
 
 
-#gitAdd(".")
-gitAdd("C:\\Users\\mmdwi\\OneDrive\\Desktop\\Gargi Documents")
-gitRepoPath = "C:\\Users\\mmdwi\\OneDrive\\Desktop\\Gargi Documents\\.git\\"
+def gitStatus():
+    cwd = pathlib.Path(os.path.abspath("."))
+    position = len(str(cwd))
 
-def getCommitId ():
+    print("\nAdded Files:")
+    counter = 0
+    for item in trackedFiles:
+        path = str(item)[position+1:]
+        print(str(counter+1) + "-> " + Fore.GREEN + path + Fore.WHITE)
+        counter = counter+1
+
+    print("\nUntracked Files:")
+    counter = 0
+    for item in untrackedFiles:
+        path = str(item)[position+1:]
+        print(str(counter+1) + "-> " + Fore.RED + path + Fore.WHITE)
+        counter = counter+1
+
+    # temp solution as hash is not stored loacally
+    # create temporary path as per your local directory
+    path1 = pathlib.Path(os.path.abspath(
+        "/Users/ashishchauhan/Desktop/Code_it/AOS/Project/demo.txt"))
+    trackingArea[path1] = "dddas"
+
+    for i in trackingArea:
+        if trackingArea[i] != shaOf(i):
+            modifiedFiles.add(i)
+
+    print("\nModified Files:")
+    counter = 0
+    for item in modifiedFiles:
+        path = str(item)[position+1:]
+        print(str(counter+1) + "-> " + Fore.YELLOW + path + Fore.WHITE)
+        counter = counter+1
+
+
+# gitAdd(".")
+p = {"demo.txt", "flow.jpeg", "demo"}  # as per the local directory
+gitAdd(p)
+
+print("\nTrackning Area : \n")
+for item in trackingArea:
+    print(trackingArea[item], "-> " + str(item))
+
+
+gitStatus()
+
+
+def getCommitId():
     t = str(time.time())
     t_encoded = t.encode("utf-8")
     soc = hashlib.sha256()
     soc.update(t_encoded)
     return soc.hexdigest()
 
-def getExtension (fileName):
+
+def getExtension(fileName):
     pos = fileName.rfind(".")
     extension = fileName[pos:]
-    #handle no extension
+    # handle no extension
     return extension
 
-def gitCommit ():
-    curr_commit_id = getCommitId ()
-    treeOfCommits[curr_commit_id] = commitHead
-    commitHead = curr_commit_id
-    index[curr_commit_id] = {}
-    for fileName in trackingArea:
-        index[curr_commit_id][fileName] = shaOf (fileName)
-        if ((trackingArea[fileName] == None) or (index[curr_commit_id][fileName] != trackingArea[fileName])):
-            trackingArea[fileName] = index[curr_commit_id][fileName]
-            extension = getExtension (fileName) #add new file to repo
-            dest = gitRepoPath + index[curr_commit_id][fileName] + extension
-            shutil.copy(fileName, dest)
-            
-
-            
+# def gitCommit():
+#     curr_commit_id = getCommitId()
+#     treeOfCommits[curr_commit_id] = commitHead
+#     commitHead = curr_commit_id
+#     index[curr_commit_id] = {}
+#     for fileName in trackingArea:
+#         index[curr_commit_id][fileName] = shaOf(fileName)
+#         if ((trackingArea[fileName] == None) or (index[curr_commit_id][fileName] != trackingArea[fileName])):
+#             trackingArea[fileName] = index[curr_commit_id][fileName]
+#             extension = getExtension(fileName)  # add new file to repo
+#             dest = gitRepoPath + index[curr_commit_id][fileName] + extension
+#             shutil.copy(fileName, dest)
--- script/driver.py
+++ script/driver.py
@@ -1,34 +1,48 @@
-import os        #for is_file(), is_dir(), abspath()
-import pathlib   #for iterdir()
+import os  # for is_file(), is_dir(), abspath()
+import pathlib  # for iterdir()
 
-trackingArea = {}   #for now
-untrackedFiles = set() #for now
+trackingArea = {}  # for now
+untrackedFiles = set()  # final set to show in git status
 
-def addDir (path):
+
+def addDir(path):
     for p in pathlib.Path(path).iterdir():
         if p.is_file() and p not in trackingArea:
             trackingArea[p] = None
-            print(p,":",trackingArea[p])
+            print(p, ":", trackingArea[p])
         elif p.is_dir():
-            addDir (p)
+            addDir(p)
 
-def addDirToUntrackedFiles (path):
+
+def addDirToUntrackedFiles(path):
     absolutePath = pathlib.Path(os.path.abspath(path))
     for p in pathlib.Path(absolutePath).iterdir():
         if p.is_file():
             untrackedFiles.add(p)
         elif p.is_dir():
-            addDirToUntrackedFiles (p)
+            addDirToUntrackedFiles(p)
 
-def gitAdd (p):
+
+def gitAdd(p):
     addDirToUntrackedFiles(".")
-    #split p
-    #for element in p
-        # absolutePath = pathlib.Path(os.path.abspath(p))
-        # if absolutePath.is_file():
-        #     trackingArea[absolutePath] = None
-        # elif absolutePath.is_dir():
-        #     addDir (absolutePath)
+    # split p
+    # for element in p
+    # absolutePath = pathlib.Path(os.path.abspath(p))
+    # if absolutePath.is_file():
+    #     trackingArea[absolutePath] = None
+    # elif absolutePath.is_dir():
+    #     addDir (absolutePath)
+
 
 gitAdd("ksjf")
 print(untrackedFiles)
+./git status
+
+
+added: git add
+
+
+untracked: git add
+
+
+modified: git satus
