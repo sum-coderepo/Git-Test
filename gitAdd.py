5d0e29d4964f67a798e5105c173c1a985214208d
Gargi Dwivedi <dwivedigargi@gmail.com>
2021-11-23 13:26:56 +0530

    git status (set difference of working directory files and tracked files)

--- gitAdd.py
+++ gitAdd.py
@@ -6,7 +6,7 @@ import time  # for time()
 from colorama import Fore
 
 trackingArea = {}       # for now
-untrackedFiles = set()  # for now
+workingDirectoryFiles = set()  # for now
 trackedFiles = set()    # for now
 modifiedFiles = set()    # for now
 index = {}              # for now
@@ -30,7 +30,7 @@ def addDir(path):
         if p.is_file() and p not in trackingArea:
             trackingArea[p] = shaOf(p)
             trackedFiles.add(p)
-            untrackedFiles.remove(p)
+            workingDirectoryFiles.remove(p)
         elif p.is_dir():
             addDir(p)
 
@@ -39,25 +39,28 @@ def addDirToUntrackedFiles(path):
     absolutePath = pathlib.Path(os.path.abspath(path))
     for p in pathlib.Path(absolutePath).iterdir():
         if p.is_file():
-            untrackedFiles.add(p)
+            workingDirectoryFiles.add(p)
         elif p.is_dir():
             addDirToUntrackedFiles(p)
 
 
 def gitAdd(p):
     # p is a list of arguments
-    addDirToUntrackedFiles(".")
     for element in p:
         absolutePath = pathlib.Path(os.path.abspath(element))
         if absolutePath.is_file():
             trackingArea[absolutePath] = shaOf(element)
             trackedFiles.add(absolutePath)
-            untrackedFiles.remove(absolutePath)
+            workingDirectoryFiles.remove(absolutePath)
         elif absolutePath.is_dir():
             addDir(absolutePath)
 
 
 def gitStatus():
+    workingDirectoryFiles.clear()
+    addDirToUntrackedFiles(".")
+    untrackedFiles = workingDirectoryFiles - trackedFiles #tracking area
+
     cwd = pathlib.Path(os.path.abspath("."))
     position = len(str(cwd))
 
@@ -92,7 +95,6 @@ def gitStatus():
         print(str(counter+1) + "-> " + Fore.YELLOW + path + Fore.WHITE)
         counter = counter+1
 
-
 # gitAdd(".")
 p = {"demo.txt", "flow.jpeg", "demo"}  # as per the local directory
 gitAdd(p)
@@ -101,9 +103,7 @@ print("\nTrackning Area : \n")
 for item in trackingArea:
     print(trackingArea[item], "-> " + str(item))
 
-
 gitStatus()
-
 
 def getCommitId():
     t = str(time.time())
@@ -112,22 +112,21 @@ def getCommitId():
     soc.update(t_encoded)
     return soc.hexdigest()
 
-
 def getExtension(fileName):
     pos = fileName.rfind(".")
     extension = fileName[pos:]
     # handle no extension
     return extension
 
-# def gitCommit():
-#     curr_commit_id = getCommitId()
-#     treeOfCommits[curr_commit_id] = commitHead
-#     commitHead = curr_commit_id
-#     index[curr_commit_id] = {}
-#     for fileName in trackingArea:
-#         index[curr_commit_id][fileName] = shaOf(fileName)
-#         if ((trackingArea[fileName] == None) or (index[curr_commit_id][fileName] != trackingArea[fileName])):
-#             trackingArea[fileName] = index[curr_commit_id][fileName]
-#             extension = getExtension(fileName)  # add new file to repo
-#             dest = gitRepoPath + index[curr_commit_id][fileName] + extension
-#             shutil.copy(fileName, dest)
+def gitCommit():
+    curr_commit_id = getCommitId()
+    treeOfCommits[curr_commit_id] = commitHead
+    commitHead = curr_commit_id
+    index[curr_commit_id] = {}
+    for fileName in trackingArea:
+        index[curr_commit_id][fileName] = shaOf(fileName)
+        if ((trackingArea[fileName] == None) or (index[curr_commit_id][fileName] != trackingArea[fileName])):
+            trackingArea[fileName] = index[curr_commit_id][fileName]
+            extension = getExtension(fileName)  # add new file to repo
+            dest = gitRepoPath + index[curr_commit_id][fileName] + extension
+            shutil.copy(fileName, dest)
